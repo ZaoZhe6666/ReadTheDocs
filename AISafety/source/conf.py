29b888bedd33a33ef449e94f7e7afaecbf01a93b
ZZ <zaozhe@buaa.edu.cn>
2020-09-25 19:07:36 +0800

    2020_0925_1907

--- AISafety/source/conf.py
+++ AISafety/source/conf.py
@@ -41,7 +41,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'recommonmark',
-    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
